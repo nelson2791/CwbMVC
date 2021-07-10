93420523e951d3066a5c89d545ff6f730b79aef6
nelson2791 <nelson2791@gmail.com>
2021-07-10 10:30:29 +0800

    加入專案檔案。

---
+++ CwbDB/WeatherForecast.sql
@@ -0,0 +1,17 @@
+﻿CREATE TABLE [dbo].[WeatherForecast]
+(
+	[seqNo] [bigint] IDENTITY(1,1) NOT NULL,
+	[datasetDescription] [nvarchar](50) NOT NULL,
+	[locationName] [nvarchar](10) NOT NULL,
+	[elementName] [varchar](20) NOT NULL,
+	[startTime] [datetime] NOT NULL,
+	[endTime] [datetime] NOT NULL,
+	[parameterName] [nvarchar](30) NOT NULL,
+	[parameterValue] [nvarchar](10) NULL,
+	[parameterUnit] [nvarchar](30) NULL,
+	[createTime] [datetime] NOT NULL,
+ CONSTRAINT [PK_WeatherForecast] PRIMARY KEY CLUSTERED 
+(
+	[seqNo] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
---
+++ CwbMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CwbMVC.MvcApplication" Language="C#" %>
